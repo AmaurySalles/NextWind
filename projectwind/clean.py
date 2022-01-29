6a62bb9aba0622d13ac44d70ce2b79f96f952cf2
AmaurySalles <amaury.salles@gmail.com>
2022-01-29 17:30:58 +0100

    Trainer.py

--- projectwind/clean.py
+++ projectwind/clean.py
@@ -1,2 +1,66 @@
-def pitch_cleaning():
+import pandas as pd
+import numpy as np
+
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import FunctionTransformer
+
+from sklearn.base import TransformerMixin, BaseEstimator
+
+from sklearn.compose import ColumnTransformer
+
+
+# Customer Transformers
+def energy_converter():
+    return ColumnTransformer(lambda x: x/6)
+
+def degree_converter(df):
+    return FunctionTransformer(lambda x: x if x <=180 else 360 - x )
+
+class Imputer(TransformerMixin, BaseEstimator):
+    def __init__(self):
+        pass
+    def fit(self, X, y=None):
+        return self
+    def transform(self, X, y=None):
+        df=X[self.list_column].interpolate()
+        return df
+
+# Pipeline
+def pipeline(data):
+
+    energy_pipe = Pipeline([
+        ('converter', energy_converter()),
+        ('imputer', Imputer()),
+        ('scaler', MinMaxScaler())
+    ])
+    
+    wind_speed_pipe = Pipeline([
+        ('imputer', Imputer()),
+        ('scaler', StandardScaler())
+    ])
+
+    nacelle_dir_pipe = Pipeline([
+        ('converter', degree_converter()),
+        ('imputer', Imputer()),
+        ('scaler', MinMaxScaler())
+
+    ])
+
+    # Paralellize "num_transformer" and "One hot encoder"
+    preprocessor = ColumnTransformer([
+        ('energy', energy_pipe, ['Power']),
+        ('wind speed', wind_speed_pipe, ['Wind Speed']),
+        ('Nacelle dir', nacelle_dir_pipe, ['Nacelle Direction'])
+    ])
+
+    return preprocessor
+
+if __name__ == "__main__":
+
+
+    pipeline.fit(all_data)
+    pipeline.transform(for i in sequence)
+    
     
---
+++ projectwind/trainer.py
@@ -0,0 +1,46 @@
+import pandas as pd
+import numpy as np
+import sklearn
+
+from projectwind.clean import clean_data
+from projectwind.clean import pipeline
+
+# Get data & split test data
+
+# Get sequence samples
+
+# Clean sequence samples (remaining missing timesteps)
+
+# Scale sequence samples
+
+# Split train/eval samples
+
+# Get model
+
+# Train model
+
+# Predict (test pipeline)
+
+
+
+def get_data():
+    data = pd.read_csv("./raw_data/A01.csv")
+    #data = clean_data(data)
+    return data
+
+def prep_data():
+    
+
+if __name__ == "__main__":
+
+    # Pipeline fit
+    full_data = get_data()
+    pipeline = pipeline()
+    pipeline.fit(full_data)
+    
+    # Sample transform
+    samples = fetch_samples()
+    scaled_sampled = []
+    for data_sample in samples:
+        scaled_sampled.append(pipeline.transform(data_sample))
+
