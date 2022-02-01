14904e09be7f35b60ce698e28e2f1a6e733b28dd
AmaurySalles <amaury.salles@gmail.com>
2022-02-01 19:25:35 +0100

    Trainer - TODO: add timestamps

--- projectwind/sampling.py
+++ projectwind/sampling.py
@@ -7,13 +7,15 @@ def get_sequences(WTG_data, day_length, number_of_subsamples, acceptable_level_o
     """
     WTG_samples = []
 
-    # Randomise start
+
     sequence_length = day_length*6*24
     last_possible_index = WTG_data.shape[0] - sequence_length
-    random_start = np.random.randint(0, last_possible_index)
-
+    
     # Select random samples with acceptable missing values levels 
     while len(WTG_samples) < number_of_subsamples :
+        
+        # Randomise start
+        random_start = np.random.randint(0, last_possible_index)
 
         # Randomly select sample            
         df_sample = WTG_data[random_start: random_start+int(sequence_length)]
@@ -34,20 +36,23 @@ def get_clean_sequences(data, fitted_pipeline, day_length, number_of_subsamples,
     '''
     sequence_length = int(day_length*6*24)
     
-    # Collect list of sequences from each WTG - returns scaled & clean data
-    sequence_list = get_sequences(data, day_length, number_of_subsamples, acceptable_level_of_missing_values)
-    
+    sample_list = []
+    for WTG_data in data:
+        # Collect list of sequences from each WTG - returns scaled & clean data
+        WTG_sample_list = get_sequences(WTG_data, day_length, number_of_subsamples/25, acceptable_level_of_missing_values)
+        sample_list.append(WTG_sample_list)
+
     # Split into 
     Y, X = [], []
-    for sample in sequence_list:
-
-        # Get target 
-        y_sample = sample['Power'].iloc[sample.shape[0]-72:]
-        Y.append(np.array(y_sample))
-        
-        # Remove target & scale data
-        X_sample = sample[0:(sequence_length -72)]
-        scaled_X_sample = fitted_pipeline.transform(X_sample)
-        X.append(scaled_X_sample)
+    for WTG_sample in sample_list:
+        for sample in WTG_sample:
+            # Get target 
+            y_sample = sample['Power'].iloc[sample.shape[0]-72:]
+            Y.append(np.array(y_sample))
+            
+            # Remove target & scale data
+            X_sample = sample[0:(sequence_length -72)]
+            scaled_X_sample = fitted_pipeline.transform(X_sample)
+            X.append(scaled_X_sample)
     
     return np.array(X), np.array(Y)
--- projectwind/trainer.py
+++ projectwind/trainer.py
@@ -10,7 +10,7 @@ def trainer():
 
     # Get data & perform  splits
     data, fit_data, = get_data()
-    data = add_timestamps(data)   # TODO Does not work - need to integrate function, so that it runs dict("WTG":pd.DataFrame)
+    #data = add_timestamps(data)   # TODO Does not work - need to integrate function, so that it runs dict("WTG":pd.DataFrame)
     train_data, test_data = split_test_data(data)
     X_fit, y_fit = split_fit_data(fit_data)
 
@@ -20,27 +20,27 @@ def trainer():
     pipeline.fit(X_fit)
     
     # Transform data & fetch sequences
-    scaled_samples = []
-    for WTG_data in train_data:
-        WTG_samples = get_clean_sequences(WTG_data,
-                                        fitted_pipeline=pipeline,
-                                        day_length=5.5, 
-                                        number_of_subsamples=100,  # Starting small with only 100 samples / WTG
-                                        acceptable_level_of_missing_values=0.1)
-        scaled_samples.append(WTG_samples)
+    samples = get_clean_sequences(train_data,
+                                fitted_pipeline=pipeline,
+                                day_length=5.5, 
+                                number_of_subsamples=100,  # Starting small with only 100 samples / WTG
+                                acceptable_level_of_missing_values=0.03)
+
     
-    print(scaled_samples)
+    print(samples)
     
     # Shuffle WTG sequences & target
     
     # Save sequences for quicker upload time
+    if fetch_data(True):
+        np.save
     # samples_csv_zip = pd.DataFrame()
     # for sample in scaled_samples:
     #     samples_csv_zip = pd.concat((samples_csv_zip,sample),ignore_index=False)
     # samples_csv_zip.to_csv('./data/samples.csv')
 
     # Load samples
-    
+    np.load
     # Get model
 
     # Train model
