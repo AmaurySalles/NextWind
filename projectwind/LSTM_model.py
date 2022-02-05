09be308d7b000a4074701f736fc1c7acf2607296
AmaurySalles <amaury.salles@gmail.com>
2022-02-05 12:15:51 +0100

    LSTM colab

--- projectwind/LSTM_model.py
+++ projectwind/LSTM_model.py
@@ -2,15 +2,15 @@ import numpy as np
 import pandas as pd
 
 from tensorflow.keras import Sequential
-from tensorflow.keras.layers import LSTM, Dense#, #BatchNormalisation
+from tensorflow.keras.layers import LSTM, Dense, BatchNormalization
 
 
 def init_LSTM_model(n_steps_in, n_steps_out, n_features):
 
     model = Sequential()
-    #model.add(BatchNormalisation())
-    model.add(LSTM(6, activation='tanh', input_shape=(n_steps_in, n_features), return_sequences=True))
-    model.add(LSTM(1, activation='tanh', return_sequences=False))
+    model.add(BatchNormalization(input_shape=(n_steps_in, n_features)))
+    model.add(LSTM(16, activation='tanh', return_sequences=False))
+    #model.add(LSTM(1, activation='tanh', return_sequences=False))
     model.add(Dense(n_steps_out, activation='linear'))
     model.compile(optimizer='adam', loss='mse')
 
