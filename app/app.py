0eeb2ffb67b926523ce8c22d2b7cf2dd2a03e0bd
hamza benkirane <hamza.benkirane@hec.edu>
2022-02-26 14:13:00 +0100

    app modified

--- app/app.py
+++ app/app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-
+import pandas as pd
 from streamlit_folium import folium_static
 import datetime
 import datetime
@@ -10,37 +10,41 @@ import folium
 import time
 import random
 
+from projectwind.predict import load_and_predict
+
 #WTG_longitude = st.number_input('pickup longitude', value=18.97466035850479)
 #WTG_latitude = st.number_input('pickup latitude', value=-69.27671861610212)
 points = []
 for i in range(25):
-    lat = random.uniform(18.96466035850479, 18.98466035850479)
-    long = random.uniform(-69.26671861610212,-69.28671861610212)
+    lat = random.uniform(18.95466035850479, 18.99466035850479)
+    long = random.uniform(-69.25671861610212,-69.29671861610212)
     coordinates = [lat,long]
     points.append(coordinates)
 
-def pinpoint_WTGs_prediction(prediction = '0KW'):
+def pinpoint_WTGs_prediction():
+    predictions = load_and_predict()
+    predictions_25 = predictions[0:25]
     WTG_longitude = 18.97466035850479
     WTG_latitude= -69.27671861610212
-    icon_url = "wind-turbine.png"
-    icon_url_red = "wind-turbine-red.png"
-    icon_url_orange = "wind-turbine-orange.png"
+    icon_url = "app/wind-turbine.png"
+    icon_url_red = "app/wind-turbine-red.png"
+    icon_url_orange = "app/wind-turbine-orange.png"
     coordinates = [WTG_longitude,WTG_latitude]
-    m = folium.Map(location=coordinates,zoom_start=14.5 ,tiles='https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
+    m = folium.Map(location=coordinates,zoom_start=13.5 ,tiles='https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
                 attr='Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community')
     for i in points:
-        if points.index(i)<19:
-            folium.Marker(i,popup=prediction,tooltip=f'WTG{points.index(i)}',icon=folium.features.CustomIcon(icon_url,icon_size=(50,50))).add_to(m)
-        elif points.index(i)<22:
-            folium.Marker(i,popup=prediction,tooltip=f'WTG{points.index(i)}',icon=folium.features.CustomIcon(icon_url_orange,icon_size=(50,50))).add_to(m)
+        if predictions_25[points.index(i)][0]==2:
+            folium.Marker(i,popup="High",tooltip=f'WTG{points.index(i)}',icon=folium.features.CustomIcon(icon_url,icon_size=(50,50))).add_to(m)
+        elif predictions_25[points.index(i)][0]==1:
+            folium.Marker(i,popup="Medium",tooltip=f'WTG{points.index(i)}',icon=folium.features.CustomIcon(icon_url_orange,icon_size=(50,50))).add_to(m)
         else:
-            folium.Marker(i,popup=prediction,tooltip=f'WTG{points.index(i)}',icon=folium.features.CustomIcon(icon_url_red,icon_size=(50,50))).add_to(m)
+            folium.Marker(i,popup="Low",tooltip=f'WTG{points.index(i)}',icon=folium.features.CustomIcon(icon_url_red,icon_size=(50,50))).add_to(m)
     return m
 
 def pinpoint_WTGs(prediction = '0KW'):
     WTG_longitude = 18.97466035850479
     WTG_latitude= -69.27671861610212
-    icon_url = "wind-turbine.png"
+    icon_url = "app/wind-turbine.png"
     coordinates = [WTG_longitude,WTG_latitude]
     m = folium.Map(location=coordinates,zoom_start=14.5, tiles='https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
                 attr='Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community')
@@ -51,7 +55,7 @@ def app():
     st.write('''
     # Projectwind model
 
-    This model predicts the energy production of 25 wind turbines
+    This model classifies the energy level production of 25 wind turbines
     ''')
 
     option = st.selectbox(
@@ -59,9 +63,9 @@ def app():
         ('None', 'Station 1', 'Station 2'))
 
     if option == 'Station 1':
-        d = st.date_input(
-        "When do you want to predict",
-         datetime.datetime(2022,2,26,18,00,00))
+#        d = st.date_input(
+#        "When do you want to predict",
+#         datetime.datetime(2022,2,26,18,00,00))
         button = st.button('Predict')
         if button:
             my_bar = st.progress(0)
@@ -69,7 +73,9 @@ def app():
             for percent_complete in range(100):
                 time.sleep(0.01)
                 my_bar.progress(percent_complete + 1)
-            st.write('Power generated for the next 12h: XXXXXXX')
+            st.write('''
+            ### The level of production of the 25 wind turbines for next 6 hours is :
+            ''')
             m = pinpoint_WTGs_prediction()
             folium_static(m)
         else:
--- app/app_main.py
+++ app/app_main.py
@@ -2,6 +2,17 @@
 import app
 import app_turbine
 import streamlit as st
+st.set_page_config(
+    page_title="ProjectWind", # => Quick reference - Streamlit
+    page_icon=":serpent:",
+    layout="wide", # wide
+    initial_sidebar_state="collapsed") # collapsed
+CSS = """
+iframe {
+    width: 100%;
+}
+"""
+st.write(f'<style>{CSS}</style>', unsafe_allow_html=True)
 PAGES = {
     "Projectwind model": app,
     "Turbine details": app_turbine
